3f2dc5f8b03963a8622b5cb4ee26321126cb6b04
nati-tewolde <natisnewemail@gmail.com>
2025-12-04 21:09:10 -0500

    Implement addLeaseContract() method in LeaseDao - Write INSERT SQL statement and extract fields from LeaseContract object - Create PreparedStatement and set parameters - Execute update and update vehicle SOLD status

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -3,9 +3,7 @@ package com.yearup.dealership.db;
 import com.yearup.dealership.models.LeaseContract;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class LeaseDao {
     private DataSource dataSource;
@@ -16,5 +14,38 @@ public class LeaseDao {
 
     public void addLeaseContract(LeaseContract leaseContract) {
         // TODO: Implement the logic to add a lease contract
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("""
+                     INSERT INTO lease_contracts
+                     (VIN, lease_start, lease_end, monthly_payment)
+                     VALUES
+                     (?, ?, ?, ?)
+                     """, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            preparedStatement.setString(1, leaseContract.getVin());
+            preparedStatement.setDate(2, Date.valueOf(leaseContract.getLeaseStart()));
+            preparedStatement.setDate(3, Date.valueOf(leaseContract.getLeaseEnd()));
+            preparedStatement.setDouble(4, leaseContract.getMonthlyPayment());
+
+            preparedStatement.executeUpdate();
+
+            // Update vehicle sold status in vehicles
+            try (PreparedStatement updateVehicle = connection.prepareStatement(
+                    "UPDATE vehicles SET SOLD = true WHERE VIN = ?")) {
+                updateVehicle.setString(1, leaseContract.getVin());
+                updateVehicle.executeUpdate();
+            }
+
+            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int contractId = generatedKeys.getInt(1);
+                    System.out.println("New Contract ID: " + contractId);
+                }
+            }
+
+        } catch (SQLException e) {
+            System.err.println("An unexpected database error occurred, please try again.");
+            e.printStackTrace();
+        }
     }
 }
